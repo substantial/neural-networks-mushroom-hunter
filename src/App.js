0161957ae1f91f45323183333900722c7aaed4cf
Adam Pearson <adam@substantial.com>
2018-06-28 16:12:55 -0700

    Add super catchy Lead

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
 import { Provider, Subscribe, Container } from 'unstated'
-import { Provider as Rebass, Heading } from 'rebass'
 
 import { TrainingReport, TrainingReportList } from './TrainingReport.js'
 import { ResultsContainer } from './ResultsContainer.js'
 import { TrainButton } from './TrainButton.js'
+
+import { Provider as Rebass, Heading, Lead, Divider } from 'rebass'
 
 class App extends Component {
   render() {
@@ -13,6 +14,8 @@ class App extends Component {
       <div className="App">
         <Rebass>
           <Heading>Mushroom Learning</Heading>
+          <Lead>Safe to eat, or deadly poison?</Lead>
+          <Divider />
           <Provider>
             <Subscribe to={[ResultsContainer]}>
               {resultsContainer => (
